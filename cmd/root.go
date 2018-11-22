881c49e8e39f4b2a071573952cea85819038fc06
Leonardo Di Donato <leodidonato@gmail.com>
2018-11-22 21:20:39 +0100

    feat(cmd): root command

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/fntlnz/kubectl-trace/pkg/cmd"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+func main() {
+	flags := pflag.NewFlagSet("kubectl-trace", pflag.ExitOnError)
+	pflag.CommandLine = flags
+
+	streams := genericclioptions.IOStreams{
+		In:     os.Stdin,
+		Out:    os.Stdout,
+		ErrOut: os.Stderr,
+	}
+	root := cmd.NewTraceCommand(streams)
+	if err := root.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
