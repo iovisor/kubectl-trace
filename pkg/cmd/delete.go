114780daafd92813cd14cbf77b05f2ff0d9b19d5
Leonardo Di Donato <leodidonato@gmail.com>
2018-11-22 21:20:06 +0100

    feat(pkg/cmd): delete subcommand

---
+++ pkg/cmd/delete.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	// "k8s.io/kubernetes/pkg/kubectl/util/templates"
+)
+
+var (
+	deleteShort = `Delete a bpftrace program execution` // Wrap with i18n.T()
+	deleteLong  = `
+...`
+
+	deleteExamples = `
+  # Delete a specific bpftrace program
+  %[1]s trace delete k656ee75a-ee3c-11e8-9e7a-8c164500a77e
+
+  # Delete all bpftrace programs in a specific namespace
+  %[1]s trace delete -n myns"`
+)
+
+// DeleteOptions ...
+type DeleteOptions struct {
+	genericclioptions.IOStreams
+}
+
+// NewDeleteOptions provides an instance of DeleteOptions with default values.
+func NewDeleteOptions(streams genericclioptions.IOStreams) *DeleteOptions {
+	return &DeleteOptions{
+		IOStreams: streams,
+	}
+}
+
+// NewDeleteCommand provides the delete command wrapping DeleteOptions.
+func NewDeleteCommand(streams genericclioptions.IOStreams) *cobra.Command {
+	o := NewDeleteOptions(streams)
+
+	cmd := &cobra.Command{
+		Use:     "delete TRACE_ID",
+		Short:   deleteShort,
+		Long:    deleteLong,                             // Wrap with templates.LongDesc()
+		Example: fmt.Sprintf(deleteExamples, "kubectl"), // Wrap with templates.Examples()
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("delete")
+			spew.Dump(o)
+		},
+	}
+
+	return cmd
+}
