dea645ff89f54d6d8356b47aa1e6e88b7193d349
Lorenzo Fontana <lo@linux.com>
2018-11-22 11:18:10 +0100

    fix(cmd/delete): more delete examples
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- cmd/kubectl-trace/delete.go
+++ cmd/kubectl-trace/delete.go
@@ -13,6 +13,13 @@ var deleteCmd = &cobra.Command{
 	Use:   "delete TRACEID",
 	Short: "Delete a trace execution from your system",
 	Long: `Delete all the running pods that are collecting your trace data using bpftrace for a given TRACEID
+
+Example:
+  # Delete a specific trace
+  kubectl trace delete 656ee75a-ee3c-11e8-9e7a-8c164500a77e
+
+Limitations:
+  This command does not implement yet a way to bulk delete traces.
 `,
 	Run: delete,
 }
@@ -21,10 +28,11 @@ func delete(cmd *cobra.Command, args []string) {
 	log, _ := zap.NewProduction()
 	defer log.Sync()
 
-	uuid := args[0]
 	if len(args) == 0 {
 		log.Fatal("TRACEID not provided")
 	}
+	uuid := args[0]
+
 	kubeconfig := viper.GetString("kubeconfig")
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 
