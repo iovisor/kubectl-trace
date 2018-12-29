432694cadab7f68e06b0cee9d2fac1bf636b967b
Lorenzo Fontana <lo@linux.com>
2018-12-29 11:00:26 +0100

    feat(ci): release docker image for PRs and master
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

---
+++ hack/release-image.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+# Script that builds the bpftrace image and releases
+# it to quay only if the request is coming from the main repo.
+
+make=$(command -v make)
+docker=$(command -v docker)
+
+$make image/build
+
+if [[ ! -z "$QUAY_TOKEN" ]]; then
+  $docker login -u="fntlnz+travisci" -p="$QUAY_TOKEN" quay.io
+  $make image/push
+
+  if [[ "TRAVIS_BRANCH" == "master" ]]; then
+    $make image/latest
+  fi
+fi
+
