229f303e2cfcad593a4a304961d54380bc070619
Lorenzo Fontana <lo@linux.com>
2019-01-09 09:18:26 +0100

    feat(integration): build images before integration tests
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- hack/ci-release-image.sh
+++ hack/ci-release-image.sh
@@ -12,8 +12,6 @@ if [[ ! -z "$TRAVIS_PULL_REQUEST_BRANCH" ]]; then
   makeopts="-e GIT_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH image/build"
 fi
 
-$make $makeopts image/build
-
 if [[ ! -z "$QUAY_TOKEN" ]]; then
   $docker login -u="fntlnz+travisci" -p="$QUAY_TOKEN" quay.io
   $make $makeopts image/push
