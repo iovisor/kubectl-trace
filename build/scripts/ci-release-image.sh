aeebd8a3f908d33ee434ed6361804e9e2ab29434
Leonardo Di Donato <leodidonato@gmail.com>
2020-10-28 19:02:39 +0000

    fix(build/scripts): check git ref is master correctly
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- build/scripts/ci-release-image.sh
+++ build/scripts/ci-release-image.sh
@@ -11,21 +11,21 @@ QUAY_BOT_USER=${QUAY_BOT_USER:-kubectltrace_buildbot}
 git_ref=$1             # github.ref        format: refs/REMOTE/REF
                        #                       eg, refs/heads/BRANCH
                        #                           refs/tags/v0.9.6-pre
-git_base_ref=$(basename ${git_ref})
+git_base_ref=$(basename "${git_ref}")
 
 make=$(command -v make)
 docker=$(command -v docker)
 
 makeopts=""
-if [[ ! -z "${git_base_ref}" ]]; then
+if [[ -n "${git_base_ref}" ]]; then
   makeopts="-e GIT_BRANCH=${git_base_ref} ${makeopts}"
 fi
 
-if [[ ! -z "$QUAY_TOKEN" ]]; then
+if [[ -n "$QUAY_TOKEN" ]]; then
   $docker login -u="${GIT_ORG}+${QUAY_BOT_USER}" -p="$QUAY_TOKEN" quay.io
-  $make $makeopts image/push
+  $make "$makeopts" image/push
 
-  if [[ "${git_ref_id}" = "master" ]] && [[ "${git_base_ref}" = "" ]]; then
-    $make $makeopts image/latest
+  if [[ "${git_base_ref}" = "master" ]]; then
+    $make "$makeopts" image/latest
   fi
 fi
